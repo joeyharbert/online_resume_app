5bd31fdfc516643f6e6eb1a467369ab4b0c3a6d2
SIeep <brandon_n_evans@live.com>
2019-04-18 19:57:08 -0500

    add embeded Twitter feed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,18 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/joeyharbert?ref_src=twsrc%5Etfw">Tweets by joeyharbert</a>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'home',
   components: {
     HelloWorld
   }
-}
+};
 </script>
