c8bcce0972c4183bfece6aed2e393b281cad6b4c
joeyharbert <joey.harbert@gmail.com>
2019-04-18 19:24:40 -0500

    Add axios and Home.vue template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+  import axios from "axios";
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      student: {}
+    };
+  },
+  created: function() {
+    axios.get('test.json').then(response => {
+      this.student = response.data;
+      console.log(this.student);
+    });
+  },
+  methods: {}
+};
 </script>
