a12602fa2a6397cfd95c3e1e7f0e15463fbc4dca
joeyharbert <joey.harbert@gmail.com>
2019-04-22 19:32:58 -0500

    Fixes image functionality

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.css'
 
 axios.defaults.baseURL = 
   process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
-require('./assets/online_resume.css')
+require('./assets/online_resume.css');
 
 Vue.config.productionTip = false
 
